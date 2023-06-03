e3c583b7100f921561f0719b7292658f4acd6806
cy-ali <92939861+cy-ali@users.noreply.github.com>
2023-06-03 12:57:47 +0200

    Initial commit for the project

---
+++ src/main/java/com/cydeo/JdAccountingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdAccountingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdAccountingProjectApplication.class, args);
+    }
+
+}
